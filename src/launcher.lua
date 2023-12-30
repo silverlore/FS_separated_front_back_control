eb0e330d08afd6a5243d94f70c3a4b5c6ffc057d
Jimmy Nielsen <silverlore@silverdragons.dk>
2023-12-30 10:41:36 +0100

    initial setup completed

---
+++ src/launcher.lua
@@ -0,0 +1,25 @@
+local directory = g_currentModDirectory
+local modName = g_currentModName
+
+local vehicles = {}
+local vehiclesByReplaceType = {}
+
+local function validateVehicleTypes(typeManager)
+    print(typeManager.typeName)
+    if typeManager.typeName == "vehicle" then
+        print("Lift/lower implement extention: start vehicleTypesValidation.")
+        FrontBackControl.modName = modName
+
+        for typeName, typeEntry in pairs(g_vehicleTypeManager:getTypes()) do 
+            if SpecializationUtil.hasSpecialization(AttacherJoints, typeEntry.specializations) and
+                not SpecializationUtil.hasSpecialization(FrontBackControl, typeEntry.specializations) then
+                    typeManager:addSpecialization(typeName, modName .. ".FrontBackControl")
+            end
+        end
+    end
+end
+
+local function init()
+    print("Lift/lower implement extention: started mod.")
+    TypeManager.validateTypes = Utils.prependedFunction(TypeManager.validateTypes, validateVehicleTypes)
+
