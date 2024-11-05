7ab7db5d72734cd5e28550e2cf8671cc3eb42162
silverlore <fragithub@silverdragons.dk>
2024-11-05 22:51:10 +0100

    final working with printing logging messages

--- src/vehicles/specialization/FrontBackControl.lua
+++ src/vehicles/specialization/FrontBackControl.lua
@@ -13,6 +13,10 @@ end
 
 function FrontBackControl.registerFunctions(vehicleType)
     SpecializationUtil.registerFunction(vehicleType, "actionHandleLowering", FrontBackControl.actionHandleLowering)
+    SpecializationUtil.registerFunction(vehicleType, "doLowering", FrontBackControl.doLowering)
+end
+
+function FrontBackControl.registerOverwrittenFunctions(vehicleType)
 end
 
 function FrontBackControl:onLoad(savegame)
@@ -70,13 +74,11 @@ function FrontBackControl:onRegisterActionEvents(isActionForInput, isActiveForIn
 end
 
 function FrontBackControl:actionHandleLowering(actionName, inputValue, callbackState, isAnalog)
-    -- print("action recieved name: " .. actionName)
-    -- print("test inputAction: " .. InputAction.SFBC_TOGGLE_FRONT)
+    print("action recieved name: " .. actionName)
+    print("test inputAction: " .. InputAction.SFBC_TOGGLE_FRONT)
     local spec = self["spec_" .. FrontBackControl.modName .. ".frontBackControl"]
     local attacherJoints_spec  = self.spec_attacherJoints
     local direction = nil
-
-    direction = not attacherJoint.moveDown
 
     if actionName == InputAction.SFBC_TOGGLE_FRONT or actionName == InputAction.SFBC_LIFT_FRONT or actionName == InputAction.SFBC_LOWER_FRONT then
         if table.getn(spec.frontJoints) > 0 then
@@ -92,11 +94,23 @@ function FrontBackControl:actionHandleLowering(actionName, inputValue, callbackS
     elseif actionName == InputAction.SFBC_TOGGLE_BACK or actionName == InputAction.SFBC_LIFT_BACK or actionName == InputAction.SFBC_LOWER_BACK then
         if table.getn(spec.backJoints) > 0 then
             if actionName == InputAction.SFBC_LIFT_BACK then
+                print("static direction lift")
                 direction = false
             elseif actionName == InputAction.SFBC_LOWER_BACK then
+                print("static direction lower")
                 direction = true
             end
+
+
+            if direction ~= nil then
+                print("direction " .. direction)
+            else
+                print("direction nil")
+            end
+
+
             for _, joint in pairs(spec.backJoints) do
+                print("Back handler joint " .. joint.jointIndex)
                 self:doLowering(joint, direction)
             end
         end
@@ -104,17 +118,37 @@ function FrontBackControl:actionHandleLowering(actionName, inputValue, callbackS
 end
 
 function FrontBackControl:doLowering(joint, direction)
+    print("doLowering. ")
     local implement = self:getImplementFromAttacherJointIndex(joint.jointIndex)
     if implement ~= nil then
+        print("doLowering. implement found.")
         if implement.object.setLoweredAll ~= nil then
+            print("doLowering. setLoweredAll found.")
             local attacherJoint = self.spec_attacherJoints.attacherJoints[joint.jointIndex]
             local doLowering = nil
             if direction ~= nil then
+                print("direction specified")
                 doLowering = direction
             else
-                doLowering = not attacherJoint.moveDown
+                if implement.object.getToggledFoldMiddleDirection ~= nil then
+                    print("getting direction from getToggledFoldMiddleDirection ")
+                    local foldDirection = implement.object:getToggledFoldMiddleDirection()
+                    print ("doLowering From ToggledFoldMiddle Direction " .. foldDirection)
+                    if foldDirection ~= 0 then
+                        doLowering = foldDirection < 0;
+                    end
+                end
+                if doLowering == nil then
+                    print ("default direction handling")
+                    doLowering = not attacherJoint.moveDown
+                end
             end
-            implement.object.setLoweredAll(doLowering, joint.jointIndex)
+            if doLowering then 
+                print("do lowering direction lower" )
+            else
+                print("do lowering direction lift" )
+            end
+            implement.object:setLoweredAll(doLowering, joint.jointIndex)
         end
     end
 end
